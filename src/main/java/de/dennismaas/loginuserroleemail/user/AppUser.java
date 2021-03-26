d0a18139d74caa1ad61fc189d3bdcd479e8978b6
DennisMaas <mail@dennismaas.de>
2021-03-26 15:39:36 +0100

    add basic user model

---
+++ src/main/java/de/dennismaas/loginuserroleemail/user/AppUser.java
@@ -0,0 +1,14 @@
+package de.dennismaas.loginuserroleemail.user;
+
+public class AppUser {
+
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private AppUserRole appUserRole;
+    private Boolean locked;
+    private Boolean enabled;
+
+}
---
+++ src/main/java/de/dennismaas/loginuserroleemail/user/AppUserRole.java
@@ -0,0 +1,6 @@
+package de.dennismaas.loginuserroleemail.user;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
