fd7260452bc2f42d035198fa1e548c9994235ccf
DennisMaas <mail@dennismaas.de>
2021-03-26 16:31:18 +0100

    add annotation to AppUserRegistrationService add return value to AppUserRegistrationService

--- src/main/java/de/dennismaas/loginuserroleemail/registration/AppUserRegistrationService.java
+++ src/main/java/de/dennismaas/loginuserroleemail/registration/AppUserRegistrationService.java
@@ -1,7 +1,10 @@
 package de.dennismaas.loginuserroleemail.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AppUserRegistrationService {
     public String register(AppUserRegistrationRequest request) {
-        return null;
+        return "works";
     }
 }
