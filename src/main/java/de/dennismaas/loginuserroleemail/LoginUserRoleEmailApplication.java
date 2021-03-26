0d19eb1321d68ee4bc1786700e93fdd0c30e8361
DennisMaas <mail@dennismaas.de>
2021-03-26 15:34:37 +0100

    Initial commit

---
+++ src/main/java/de/dennismaas/loginuserroleemail/LoginUserRoleEmailApplication.java
@@ -0,0 +1,13 @@
+package de.dennismaas.loginuserroleemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginUserRoleEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginUserRoleEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dennismaas/loginuserroleemail/LoginUserRoleEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dennismaas.loginuserroleemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginUserRoleEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
