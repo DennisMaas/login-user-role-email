f64e6b52bf75eea321734439c6b760ec42702df0
DennisMaas <mail@dennismaas.de>
2021-03-27 20:36:08 +0100

    add ConfirmationTokenRepository.java

---
+++ src/main/java/de/dennismaas/loginuserroleemail/registration/token/ConfirmationTokenRepository.java
@@ -0,0 +1,12 @@
+package de.dennismaas.loginuserroleemail.registration.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
+}
