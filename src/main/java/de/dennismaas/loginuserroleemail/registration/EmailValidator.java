c583d20b611e1b80fd635a32944ddea2973594e0
DennisMaas <mail@dennismaas.de>
2021-03-27 19:53:59 +0100

    add service annotation

--- src/main/java/de/dennismaas/loginuserroleemail/registration/EmailValidator.java
+++ src/main/java/de/dennismaas/loginuserroleemail/registration/EmailValidator.java
@@ -1,11 +1,13 @@
 package de.dennismaas.loginuserroleemail.registration;
 
+import org.springframework.stereotype.Service;
+
 import java.util.function.Predicate;
 /*
 import org.apache.commons.validator.routines.EmailValidator;
 */
 
-
+@Service
 public class EmailValidator implements Predicate<String> {
     @Override
     public boolean test(String email) {
