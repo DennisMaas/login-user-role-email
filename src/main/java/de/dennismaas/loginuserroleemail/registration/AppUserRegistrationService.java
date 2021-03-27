ce7966382102efb451bfb0fee911ad58bbec4a7e
DennisMaas <mail@dennismaas.de>
2021-03-27 19:53:18 +0100

    add method to register user

--- src/main/java/de/dennismaas/loginuserroleemail/registration/AppUserRegistrationService.java
+++ src/main/java/de/dennismaas/loginuserroleemail/registration/AppUserRegistrationService.java
@@ -1,10 +1,31 @@
 package de.dennismaas.loginuserroleemail.registration;
 
+
+import de.dennismaas.loginuserroleemail.appuser.AppUser;
+import de.dennismaas.loginuserroleemail.appuser.AppUserRole;
+import de.dennismaas.loginuserroleemail.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AppUserRegistrationService {
-    public String register(AppUserRegistrationRequest request) {
-        return "works";
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register (AppUserRegistrationRequest request){
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+
+        ));
     }
 }
