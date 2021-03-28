9f64965284cc62ff4e7ecd5ebd449bdcf33a0bd2
DennisMaas <mail@dennismaas.de>
2021-03-28 20:32:19 +0200

    add regex in EmailValidator

--- src/main/java/de/dennismaas/loginuserroleemail/registration/EmailValidator.java
+++ src/main/java/de/dennismaas/loginuserroleemail/registration/EmailValidator.java
@@ -3,16 +3,24 @@ package de.dennismaas.loginuserroleemail.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
-/*
-import org.apache.commons.validator.routines.EmailValidator;
-*/
+
 
 @Service
 public class EmailValidator implements Predicate<String> {
+
+    /*
+    * This regex example uses all the characters permitted by RFC 5322,
+    * which governs the email message format. Among the permitted characters are
+    * some that present a security risk if passed directly from user input to an SQL statement,
+    * such as the single quote (‘) and the pipe character (|).
+    * You should be sure to escape sensitive characters when inserting the email address
+    * into a string passed to another program, in order to prevent security holes such as SQL injection attacks.
+    */
+    private final String EMAIL_PATTERN = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+
     @Override
     public boolean test(String email) {
-        //TODO: Regex to validate
-        // String regex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-        return true;
+
+        return email.matches(EMAIL_PATTERN);
     }
 }
